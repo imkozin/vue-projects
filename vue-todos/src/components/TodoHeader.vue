0a897bdb784deb9c220063811ccab6044f30c0c2
imkozin <ivan.kozin@hotmail.com>
2023-12-18 16:51:07 +0200

    created project

---
+++ vue-todos/src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <header>
+        <nav class="container">
+            <div class="branding">
+                <img src="" alt="">
+            </div>
+        </nav>
+    </header>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
