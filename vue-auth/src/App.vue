bb2a836070adc05e3e9a1dda8be1147383478b0e
imkozin <ivan.kozin@hotmail.com>
2024-02-21 14:45:05 +0300

    init

---
+++ vue-auth/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <form class="card">
+      <h1>Auth</h1>
+
+      <div class="form-control" >
+        <label for="email">Email</label>
+        <input type="email" id="email" >
+      </div>
+
+      <div class="form-control" >
+        <label for="password">Password</label>
+        <input type="password" id="password" >
+      </div>
+
+      <button class="btn primary" type="submit">Submit</button>
+    </form>
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
