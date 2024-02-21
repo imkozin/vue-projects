d63a503784a7cd9fd8bb96a4cf3551a0a020c282
imkozin <ivan.kozin@hotmail.com>
2024-02-21 15:49:25 +0300

    init

--- vue-todos/src/App.vue
+++ vue-todos/src/App.vue
@@ -1,25 +1,26 @@
-<script setup>
-import {  RouterView } from 'vue-router'
-import TodoHeader from './components/TodoHeader.vue';
-</script>
-
 <template>
-  <TodoHeader/>
-  <RouterView />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap');
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-* {
-  font-family: "Rubik", sans-serif;
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
+</script>
 
-.container {
-  max-width: 1100px;
-  margin: 0 auto;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
