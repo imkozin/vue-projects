c8506e738146625d1ade1e9a7cf050ee4f6d1943
imkozin <ivan.kozin@hotmail.com>
2024-02-25 14:42:12 +0300

    computed, watch

--- vue-composition/src/views/HomeView.vue
+++ vue-composition/src/views/HomeView.vue
@@ -1,26 +1,82 @@
 <template>
   <div class="home">
+    <h2>{{ appTitle }}</h2>
+    <h3>{{ counterData.title }}</h3>
     <div>
-      <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
-      <button @click="increaseCounter" class="btn">+</button>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
+    </div>
+
+    <p>This counter is {{ oddOrEven }}</p>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { computed, reactive, ref, watch } from 'vue'
 
-const counter = ref(0)
+// const counter = ref(0),
+//   counterTitle = ref('My Counter')
+const appTitle = 'My Amazing Counter App'
 
-const increaseCounter = () => {
-  counter.value++
+const counterData = reactive({
+  count: 0,
+  title: 'My Counter',
+})
+
+watch(() => counterData.count, (newCount, oldCount) => {
+   if(newCount == 20) {
+    alert('You reached 20')
+   }
+  }
+)
+
+const oddOrEven = computed(() => {
+  if (counterData.count % 2 === 0) return 'even'
+  return 'odd'
+})
+
+const increaseCounter = (amount, e) => {
+  console.log(e)
+  // counter.value++
+  // counterData.count++
+  counterData.count += amount
 }
 
-const decreaseCounter = () => {
-  counter.value--
+const decreaseCounter = (amount) => {
+  // counter.value--
+  // counterData.count--
+  counterData.count -= amount
 }
 </script>
+
+<!-- <script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  computed: {
+    myCompProp() {
+      return 'my result'
+    }
+  },
+  watch: {
+    count(newCount, oldCount) {
+      if (newCount == 20) alert('new message')
+    }
+  }
+}
+</script> 
+-->
 
 <!-- <script >
 import { ref } from 'vue'
@@ -74,5 +130,9 @@ export default {
 .counter {
   font-size: 40px;
   margin: 10px;
+}
+
+.edit {
+  margin-top: 60px;
 }
 </style>
